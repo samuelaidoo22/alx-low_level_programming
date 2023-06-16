ec2b244ccab5d94e77c4c040239d0fa093af3aac
Samuel Aidoo <augutine22@gmail.com>
2023-06-16 07:02:00 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - a program the print a line using the printf function
- * return: 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n")
 		return (0);
 }
