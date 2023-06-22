ab992c708c829c982b365238fc012e8ea75a55a5
Samuel Aidoo <augutine22@gmail.com>
2023-06-22 07:39:13 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number, sum;
+
+	sum = 0;
+
+	for (number = 0; number < 1024; number++)
+	{
+		if (number % 3 == 0 || number % 5 == 0)
+			sum += number;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
